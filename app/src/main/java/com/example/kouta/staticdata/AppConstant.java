5e555307c7721977e82799afb814da6ad1632633
wf <waylen1009@163.com>
2021-05-31 11:36:33 +0800

    7-9

--- app/src/main/java/com/example/kouta/staticdata/AppConstant.java
+++
@@ -1,9 +0,0 @@
-package com.example.kouta.staticdata;
-
-public class AppConstant {
-    public static String endPoint = "obs.cn-east-3.myhuaweicloud.com";
-    public static String ak = "IP3GPV0JTU2VPFT8MCQ7";
-    public static String sk = "3qDRxizVN8ukaMfPPpqtvprzr34TEx7BpwhM1jhC";
-    public static String bucketName = "ahxd-private";
-
-}
