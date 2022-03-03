f7c1764b94dce496968bbd92d48b1f67a38a4287
ouyang <675827586@qq.com>
2022-03-03 11:34:25 +0800

    优化

--- app/src/main/java/com/xunda/mo/hx/section/chat/views/MyEaseChatRowFile.java
+++ app/src/main/java/com/xunda/mo/hx/section/chat/views/MyEaseChatRowFile.java
@@ -2,19 +2,40 @@ package com.xunda.mo.hx.section.chat.views;
 
 import android.content.Context;
 import android.net.Uri;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.TextView;
 
+import androidx.core.content.ContextCompat;
+
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.bumptech.glide.request.RequestOptions;
+import com.hyphenate.chat.EMClient;
+import com.hyphenate.chat.EMConversation;
 import com.hyphenate.chat.EMMessage;
 import com.hyphenate.chat.EMNormalFileMessageBody;
+import com.hyphenate.easeui.modules.chat.EaseChatMessageListLayout;
+import com.hyphenate.easeui.modules.chat.model.EaseChatItemStyleHelper;
 import com.hyphenate.easeui.utils.EaseEditTextUtils;
 import com.hyphenate.easeui.utils.EaseFileUtils;
+import com.hyphenate.easeui.utils.EaseUserUtils;
+import com.hyphenate.easeui.utils.GsonUtil;
+import com.hyphenate.easeui.utils.ListUtils;
+import com.hyphenate.easeui.utils.StringUtil;
 import com.hyphenate.easeui.widget.chatrow.EaseChatRow;
 import com.hyphenate.util.TextFormater;
 import com.xunda.mo.R;
 import com.xunda.mo.main.constant.MyConstant;
+import com.xunda.mo.main.info.MyInfo;
+import com.xunda.mo.model.GroupMember_Bean;
 import com.xunda.mo.network.saveFile;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * file for row
@@ -29,6 +50,7 @@ public class MyEaseChatRowFile extends EaseChatRow {
      * file's size
      */
     protected TextView fileSizeView;
+    private TextView tv_user_role,tv_vip;
     /**
      * file state
      */
@@ -51,10 +73,12 @@ public class MyEaseChatRowFile extends EaseChatRow {
 
     @Override
     protected void onFindViewById() {
-        fileNameView = (TextView) findViewById(R.id.tv_file_name);
-        fileSizeView = (TextView) findViewById(R.id.tv_file_size);
-        fileStateView = (TextView) findViewById(R.id.tv_file_state);
-        percentageView = (TextView) findViewById(R.id.percentage);
+        fileNameView = findViewById(R.id.tv_file_name);
+        fileSizeView = findViewById(R.id.tv_file_size);
+        fileStateView = findViewById(R.id.tv_file_state);
+        percentageView = findViewById(R.id.percentage);
+        tv_user_role = findViewById(R.id.tv_user_role);
+        tv_vip = findViewById(com.xunda.mo.R.id.tv_vip);
     }
 
     @Override
@@ -86,20 +110,105 @@ public class MyEaseChatRowFile extends EaseChatRow {
             }
         }
 
-        if (message.getChatType() == EMMessage.ChatType.Chat) {
-            if (isSender()){
-                String headUrl = message.getStringAttribute(MyConstant.SEND_HEAD, "");
-                Glide.with(getContext()).load(headUrl).into(userAvatarView);
+        if (isSender()) {
+            MyInfo info = new MyInfo(context);
+            try {
+                int avatarResId = Integer.parseInt(info.getUserInfo().getHeadImg());
+                Glide.with(context).load(avatarResId).into(userAvatarView);
+            } catch (Exception e) {
+                Glide.with(context).load(info.getUserInfo().getHeadImg())
+                        .apply(RequestOptions.placeholderOf(R.mipmap.img_pic_none)
+                                .diskCacheStrategy(DiskCacheStrategy.ALL))
+                        .into(userAvatarView);
             }
-            //添加群聊其他用户的名字与头像
-        }else if (message.getChatType() == EMMessage.ChatType.GroupChat) {
-            usernickView.setText(message.getStringAttribute(MyConstant.SEND_NAME,""));
-            String headUrl = message.getStringAttribute(MyConstant.SEND_HEAD,"");
-            Glide.with(getContext()).load(headUrl).placeholder(R.drawable.mo_icon).into(userAvatarView);
+            //只要不是常规布局形式，就展示昵称
+            if (EaseChatItemStyleHelper.getInstance().getStyle().getItemShowType() != EaseChatMessageListLayout.ShowType.NORMAL.ordinal()) {
+                EaseUserUtils.setUserNick(message.getFrom(), usernickView);
+            }
+        } else {
+            //添加群其他用户的名字与头像
+            if (message.getChatType() == EMMessage.ChatType.GroupChat) {
+                EMConversation conversation = EMClient.getInstance().chatManager().getConversation(message.conversationId());
+                String extMessage = conversation.getExtField();
+                List<GroupMember_Bean.DataDTO> memberList = new ArrayList<>();
+                if (!TextUtils.isEmpty(extMessage)) {
+                    JSONObject JsonObject = null;
+                    try {
+                        JsonObject = new JSONObject(extMessage);
+                        boolean isInsertGroupOrFriendInfo = JsonObject.getBoolean("isInsertGroupOrFriendInfo");
+                        if (isInsertGroupOrFriendInfo) {
+                            String jsonMemberList = JsonObject.getString("groupMemberList");
+                            GroupMember_Bean groupListModel = GsonUtil.getInstance().json2Bean(jsonMemberList, GroupMember_Bean.class);
+                            if (groupListModel != null) {
+                                memberList.addAll(groupListModel.getData());
+                            }
+                        }
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
 
-            //匿名聊天
-            if (!saveFile.getShareData(MyConstant.GROUP_CHAT_ANONYMOUS + message.conversationId(), context).equals("false")) {
-                Glide.with(getContext()).load(R.drawable.anonymous_chat_icon).placeholder(R.drawable.mo_icon).into(userAvatarView);
+                String name = message.getStringAttribute(MyConstant.SEND_NAME, "");
+                String headUrl = message.getStringAttribute(MyConstant.SEND_HEAD, "");
+                int vipType = message.getIntAttribute(MyConstant.SEND_VIP, 3);
+
+                if (!ListUtils.isEmpty(memberList)) {
+                    for (GroupMember_Bean.DataDTO memberObj : memberList) {
+                        if (memberObj.getHxUserName().equals(message.getFrom())) {
+                            name = memberObj.getNickname();//在群会话页显示的昵称
+                            headUrl = memberObj.getHeadImg();
+                            message.setAttribute(MyConstant.SEND_NAME, name);
+                            if (tv_user_role != null) {
+                                if (memberObj.getIdentity() == 1) {
+                                    tv_user_role.setVisibility(View.VISIBLE);
+                                    tv_user_role.setText("群主");
+                                    tv_user_role.setBackgroundResource(R.drawable.shape_bg_all_member_qunzhu);
+                                    tv_user_role.setTextColor(ContextCompat.getColor(context, R.color.color_FB8717));
+                                } else if (memberObj.getIdentity() == 2) {
+                                    tv_user_role.setVisibility(View.VISIBLE);
+                                    tv_user_role.setText("管理员");
+                                    tv_user_role.setBackgroundResource(R.drawable.shape_bg_all_member_guanliyuan);
+                                    tv_user_role.setTextColor(ContextCompat.getColor(context, R.color.color_2391F3));
+                                } else {
+                                    tv_user_role.setVisibility(View.GONE);
+                                }
+                            }
+
+                            if (vipType == 3) {
+                                vipType = memberObj.getVipType();
+                            }
+
+                            if (vipType == 1) {
+                                usernickView.setTextColor(ContextCompat.getColor(context, R.color.yellowfive));
+                                if (tv_vip != null) {
+                                    tv_vip.setVisibility(VISIBLE);
+                                }
+                            } else {
+                                usernickView.setTextColor(ContextCompat.getColor(context, R.color.greytwo));
+                                if (tv_vip != null) {
+                                    tv_vip.setVisibility(GONE);
+                                }
+                            }
+                            break;
+                        }
+                    }
+                }
+
+                if (StringUtil.isBlank(name)) {
+                    EaseUserUtils.setUserNick(message.getFrom(), usernickView);
+                } else {
+                    usernickView.setText(name);
+                }
+
+
+                if (StringUtil.isBlank(headUrl)) {
+                    EaseUserUtils.setUserAvatar(context, message.getFrom(), userAvatarView);
+                } else {
+                    Glide.with(getContext()).load(headUrl).placeholder(R.mipmap.img_pic_none).error(R.mipmap.img_pic_none).into(userAvatarView);
+                }
+            } else {
+                String header_url_message = message.getStringAttribute(MyConstant.SEND_HEAD, "");
+                EaseUserUtils.setUserAvatarAndSendHeaderUrl(context, message.getFrom(), header_url_message, userAvatarView);
             }
         }
     }
